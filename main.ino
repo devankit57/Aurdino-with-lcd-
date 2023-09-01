58d139a9fd05d5ab6115afb2c915145b13ad65ee
Ankit <83949374+devankit57@users.noreply.github.com>
2023-09-01 14:11:09 +0530

    Create main.ino

---
+++ main.ino
@@ -0,0 +1,31 @@
+#include <Wire.h> 
+#include <LiquidCrystal_I2C.h>
+
+// Set the LCD address to 0x27 for a 16 chars and 2 line display
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+
+void setup()
+{
+	// initialize the LCD
+	lcd.begin();
+
+	// Turn on the blacklight and print a message.
+	lcd.backlight();
+  for(int i=0;i<1000;i++){
+    lcd.print("Jai Shree Ram");
+  
+  delay(1000);
+  lcd.clear();
+  lcd.print("Sita Ram");
+  delay(1000);
+  lcd.clear();
+  }
+	
+  
+  
+}
+
+void loop()
+{
+	// Do nothing here...
+}
